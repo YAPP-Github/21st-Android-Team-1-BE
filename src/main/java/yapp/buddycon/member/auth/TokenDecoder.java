46a015f62690b932125d5ce8cb17b5c51cdeb64e
yubin <dbqls2821@gmail.com>
2023-01-02 17:26:54 +0900

    feat: jwt token decode 로직 추가 & 에러 처리

---
+++ src/main/java/yapp/buddycon/member/auth/TokenDecoder.java
@@ -0,0 +1,28 @@
+package yapp.buddycon.member.auth;
+
+import io.jsonwebtoken.*;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import yapp.buddycon.exception.CustomException;
+import yapp.buddycon.exception.ErrorCode;
+
+@Component
+public class TokenDecoder {
+
+  @Value("${security.jwt.token.secret-key}")
+  private String SECRET_KEY;
+
+  public String decode(String accessToken) {
+    try {
+      return Jwts.parserBuilder().setSigningKey(SECRET_KEY).build().parseClaimsJws(accessToken).getBody().get("memberId").toString();
+    } catch (ExpiredJwtException e) {
+      throw new CustomException(ErrorCode.EXPIRED_ACCESS_TOKEN);
+    } catch (SecurityException | MalformedJwtException e) {
+      throw new CustomException(ErrorCode.MALFORMED_JWT_TOKEN);
+    } catch (UnsupportedJwtException e) {
+      throw new CustomException(ErrorCode.UNSUPPORTED_JWT_TOKEN);
+    } catch (IllegalArgumentException e) {
+      throw new CustomException(ErrorCode.INVALID_JWT_TOKEN);
+    }
+  }
+}
