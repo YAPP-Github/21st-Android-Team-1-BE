c9c4409e0f13cd0265c485723162d6565629f2e4
yubin <dbqls2821@gmail.com>
2023-01-02 17:22:20 +0900

    feat: resolveArgument() 메소드 내부 구현

--- src/main/java/yapp/buddycon/member/auth/AuthenticationArgumentResolver.java
+++ src/main/java/yapp/buddycon/member/auth/AuthenticationArgumentResolver.java
@@ -1,23 +1,33 @@
 package yapp.buddycon.member.auth;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.MethodParameter;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.support.WebDataBinderFactory;
 import org.springframework.web.context.request.NativeWebRequest;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.method.support.ModelAndViewContainer;
+import yapp.buddycon.exception.CustomException;
+import yapp.buddycon.exception.ErrorCode;
 
+@Slf4j
 @Component
+@RequiredArgsConstructor
 public class AuthenticationArgumentResolver implements HandlerMethodArgumentResolver {
+
+  private final AuthMemberProvider authMemberProvider;
+
   @Override
   public boolean supportsParameter(MethodParameter parameter) {
-    final boolean isRegUserAnnotation = parameter.getParameterAnnotation(AuthMember.class) != null;
-    final boolean isRegisterDto = parameter.getParameterType().equals(AuthMember.class);
-    return isRegUserAnnotation && isRegisterDto;
+    return parameter.getParameterType() == AuthMember.class;
   }
 
   @Override
   public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
+    String token = webRequest.getHeader("Authorization");
+    if (token == null) throw new CustomException(ErrorCode.NOT_EXIST_ACCESS_TOKEN);
+    authMemberProvider.getAuthMember(token.substring(7));
     return null;
   }
 }
