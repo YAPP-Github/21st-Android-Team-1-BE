d8c3b08b97d3a4772d83c37ed3425e461430ad99
yubin <dbqls2821@gmail.com>
2023-01-02 19:59:06 +0900

    feat: ErrorCode 추가

--- src/main/java/yapp/buddycon/exception/ErrorCode.java
+++ src/main/java/yapp/buddycon/exception/ErrorCode.java
@@ -17,6 +17,8 @@ public enum ErrorCode {
   MALFORMED_JWT_TOKEN(BAD_REQUEST, "잘못된 JWT 서명입니다."),
   UNSUPPORTED_JWT_TOKEN(BAD_REQUEST, "지원되지 않는 JWT 토큰입니다."),
   INVALID_JWT_TOKEN(BAD_REQUEST, "JWT 토큰이 잘못되었습니다."),
+  LOGGED_OUT_MEMBER(BAD_REQUEST, "로그아웃된 사용자입니다."),
+  TOKEN_MEMBER_INFO_IS_NOT_MATCH(BAD_REQUEST, "토큰의 사용자 정보가 일치하지 않습니다."),
 
   ;
 
