259b6f849e7dec52b647d76f5dc7b194aa21a946
yubin <dbqls2821@gmail.com>
2023-01-02 19:58:53 +0900

    feat: redis serializer 추가

--- src/main/java/yapp/buddycon/config/RedisConfig.java
+++ src/main/java/yapp/buddycon/config/RedisConfig.java
@@ -8,6 +8,7 @@ import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 @RequiredArgsConstructor
@@ -25,15 +26,15 @@ public class RedisConfig {
     return new LettuceConnectionFactory(host, port);
   }
 
-//  @Bean
-//  public RedisConnectionFactory redisConnectionFactory() {
-//    return new LettuceConnectionFactory(redisProperties.getHost(), redisProperties.getPort());
-//  }
-
   @Bean
   public RedisTemplate<?, ?> redisTemplate() {
     RedisTemplate<byte[], byte[]> template = new RedisTemplate<>();
     template.setConnectionFactory(redisConnectionFactory());
+    template.setHashValueSerializer(new StringRedisSerializer());
+    template.setKeySerializer(new StringRedisSerializer());
+    template.setValueSerializer(new StringRedisSerializer());
+    template.setHashKeySerializer(new StringRedisSerializer());
+    template.afterPropertiesSet();
     return template;
   }
 }
