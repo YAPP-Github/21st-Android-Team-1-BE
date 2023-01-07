2adb74bfe39901068f7e6ea71b1cace03e6a87a7
yubin <dbqls2821@gmail.com>
2023-01-07 21:08:00 +0900

    fix: redis 설정 임시 제거 & secret key 제거한 application.yml 생성

--- src/main/java/yapp/buddycon/config/RedisConfig.java
+++ src/main/java/yapp/buddycon/config/RedisConfig.java
@@ -1,40 +1,40 @@
-package yapp.buddycon.config;
-
-import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
-import org.springframework.data.redis.serializer.StringRedisSerializer;
-
-@Configuration
-@RequiredArgsConstructor
-@EnableRedisRepositories
-public class RedisConfig {
-
-  @Value("${spring.redis.host}")
-  private String host;
-
-  @Value("${spring.redis.port}")
-  private int port;
-
-  @Bean
-  public RedisConnectionFactory redisConnectionFactory() {
-    return new LettuceConnectionFactory(host, port);
-  }
-
-  @Bean
-  public RedisTemplate<?, ?> redisTemplate() {
-    RedisTemplate<byte[], byte[]> template = new RedisTemplate<>();
-    template.setConnectionFactory(redisConnectionFactory());
-    template.setHashValueSerializer(new StringRedisSerializer());
-    template.setKeySerializer(new StringRedisSerializer());
-    template.setValueSerializer(new StringRedisSerializer());
-    template.setHashKeySerializer(new StringRedisSerializer());
-    template.afterPropertiesSet();
-    return template;
-  }
-}
+//package yapp.buddycon.config;
+//
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.data.redis.connection.RedisConnectionFactory;
+//import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+//import org.springframework.data.redis.core.RedisTemplate;
+//import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+//import org.springframework.data.redis.serializer.StringRedisSerializer;
+//
+//@Configuration
+//@RequiredArgsConstructor
+//@EnableRedisRepositories
+//public class RedisConfig {
+//
+//  @Value("${spring.redis.host}")
+//  private String host;
+//
+//  @Value("${spring.redis.port}")
+//  private int port;
+//
+//  @Bean
+//  public RedisConnectionFactory redisConnectionFactory() {
+//    return new LettuceConnectionFactory(host, port);
+//  }
+//
+//  @Bean
+//  public RedisTemplate<?, ?> redisTemplate() {
+//    RedisTemplate<byte[], byte[]> template = new RedisTemplate<>();
+//    template.setConnectionFactory(redisConnectionFactory());
+//    template.setHashValueSerializer(new StringRedisSerializer());
+//    template.setKeySerializer(new StringRedisSerializer());
+//    template.setValueSerializer(new StringRedisSerializer());
+//    template.setHashKeySerializer(new StringRedisSerializer());
+//    template.afterPropertiesSet();
+//    return template;
+//  }
+//}
