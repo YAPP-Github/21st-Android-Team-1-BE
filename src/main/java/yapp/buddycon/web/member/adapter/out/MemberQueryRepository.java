96fc2dea5aa17688231c24e06b660ceb571dfff8
yubin <dbqls2821@gmail.com>
2023-01-09 21:58:38 +0900

    feat: Repository클래스들 생성 & 내부 구현

---
+++ src/main/java/yapp/buddycon/web/member/adapter/out/MemberQueryRepository.java
@@ -0,0 +1,20 @@
+package yapp.buddycon.web.member.adapter.out;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+import yapp.buddycon.web.member.application.port.out.MemberQueryPort;
+import yapp.buddycon.web.member.auth.application.port.out.AuthToMemberQueryPort;
+import yapp.buddycon.web.member.domain.Member;
+
+@Repository
+@RequiredArgsConstructor
+class MemberQueryRepository implements MemberQueryPort,
+  AuthToMemberQueryPort {
+
+    private final MemberJpaRepository memberJpaRepository;
+
+    @Override
+    public Member findMemberByClientId(Long clientId) {
+        return memberJpaRepository.findMemberByClientId(clientId);
+    }
+}
