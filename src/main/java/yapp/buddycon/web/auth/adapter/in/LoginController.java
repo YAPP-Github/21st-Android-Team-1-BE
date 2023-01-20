19d300c3de94703e03014939dcd1b963242da345
huey-J <wkdgus7113@gmail.com>
2023-01-20 23:13:19 +0900

    feat: 회원 탈퇴 API 추가

--- src/main/java/yapp/buddycon/web/auth/adapter/in/LoginController.java
+++ src/main/java/yapp/buddycon/web/auth/adapter/in/LoginController.java
@@ -1,8 +1,10 @@
 package yapp.buddycon.web.auth.adapter.in;
 
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 import yapp.buddycon.web.auth.adapter.in.response.TokenResponseDto;
+import yapp.buddycon.web.auth.adapter.out.AuthMember;
 import yapp.buddycon.web.auth.application.service.AuthService;
 
 @RestController
@@ -13,6 +15,7 @@ public class LoginController {
   private final AuthService authService;
 
   @PostMapping("login/{accessToken}")
+  @Operation(summary = "로그인", description = "최초 로그인일 경우 회원가입 처리")
   public TokenResponseDto login(@PathVariable String accessToken) {
     return authService.login(accessToken);
   }
@@ -21,5 +24,11 @@ public class LoginController {
 //  public TokenResponseDto reissue(@RequestBody ReissueRequestDto reissueRequestDto) {
 //    return authService.reissue(reissueRequestDto);
 //  }
+
+  @PostMapping("unregister")
+  @Operation(summary = "회원 탈퇴")
+  public void unregister(AuthMember authMember) {
+
+  }
 
 }
