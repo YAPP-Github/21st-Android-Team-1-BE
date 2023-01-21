272f1fbf18a1d36acfba0c6a08e36b196c858734
huey-J <wkdgus7113@gmail.com>
2023-01-21 18:01:41 +0900

    refactor: NotificationResponseDto 화면에서 사용하지 않는 데이터 삭제

--- src/main/java/yapp/buddycon/web/notification/adapter/in/response/NotificationResponseDto.java
+++ src/main/java/yapp/buddycon/web/notification/adapter/in/response/NotificationResponseDto.java
@@ -1,7 +1,6 @@
 package yapp.buddycon.web.notification.adapter.in.response;
 
 import java.time.LocalDate;
-import yapp.buddycon.web.coupon.domain.CouponState;
 import yapp.buddycon.web.coupon.domain.CouponType;
 
 public record NotificationResponseDto(
@@ -13,13 +12,8 @@ public record NotificationResponseDto(
 
     // coupon
     Long couponId,
-    CouponState couponState,
     CouponType couponType,
-    String couponImageUrl,
     String couponName,
-    String couponMemo,
-    String storeName,
-    String sentMemberName,
     LocalDate expireDate
 ) {
 
