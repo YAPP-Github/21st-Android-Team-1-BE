e7b21a1b802287068d0a9d2fd666454ffa854fb0
yubin <dbqls2821@gmail.com>
2023-01-24 14:55:34 +0900

    feat: refresh token 로직 생성

--- src/main/java/yapp/buddycon/web/auth/application/service/TokenProvider.java
+++ src/main/java/yapp/buddycon/web/auth/application/service/TokenProvider.java
@@ -4,12 +4,14 @@ import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Component;
 import yapp.buddycon.web.auth.adapter.in.response.TokenResponseDto;
 
 import java.util.Base64;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.concurrent.TimeUnit;
 
 @Component
 @RequiredArgsConstructor
@@ -23,7 +25,7 @@ public class TokenProvider {
   @Value("${security.jwt.token.refresh-token-expire-time}")
   private long REFRESH_TOKEN_EXPIRE_TIME;
 
-//  private final RedisTemplate<String, Object> redisTemplate;
+  private final RedisTemplate<String, Object> redisTemplate;
 
   public TokenResponseDto createToken(Long memberId) {
     String secretKey = Base64.getEncoder().encodeToString(SECRET_KEY.getBytes());
@@ -32,28 +34,27 @@ public class TokenProvider {
     Date accessTokenExpiresIn = new Date(now.getTime() + ACCESS_TOKEN_EXPIRE_TIME);
     String accessToken = Jwts.builder()
       .setClaims(new HashMap<String, Long>(){{ put("memberId", memberId); }})
-//      .setIssuedAt(now)
+      .setIssuedAt(now)
       .setExpiration(accessTokenExpiresIn)
       .signWith(SignatureAlgorithm.HS256, secretKey)
       .compact();
-//    String refreshToken = Jwts.builder()
-//      .setExpiration(new Date(now.getTime() + REFRESH_TOKEN_EXPIRE_TIME))
-//      .signWith(SignatureAlgorithm.HS256, secretKey)
-//      .compact();
+    String refreshToken = Jwts.builder()
+      .setExpiration(new Date(now.getTime() + REFRESH_TOKEN_EXPIRE_TIME))
+      .signWith(SignatureAlgorithm.HS256, secretKey)
+      .compact();
 
-//    saveRefreshToken(memberId, refreshToken);
+    saveRefreshToken(memberId, refreshToken);
 
-//    return new TokenResponseDto(BEARER_TYPE, accessToken, refreshToken, accessTokenExpiresIn.getTime());
-    return new TokenResponseDto(BEARER_TYPE, accessToken, accessTokenExpiresIn.getTime());
+    return new TokenResponseDto(BEARER_TYPE, accessToken, refreshToken, accessTokenExpiresIn.getTime());
   }
 
-//  private void saveRefreshToken(Long memberId, String refreshToken) {
-//    redisTemplate.opsForValue()
-//      .set(
-//        "RT:" + memberId,
-//        refreshToken,
-//        REFRESH_TOKEN_EXPIRE_TIME,
-//        TimeUnit.MILLISECONDS
-//      );
-//  }
+  private void saveRefreshToken(Long memberId, String refreshToken) {
+    redisTemplate.opsForValue()
+      .set(
+        "RT:" + memberId,
+        refreshToken,
+        REFRESH_TOKEN_EXPIRE_TIME,
+        TimeUnit.MILLISECONDS
+      );
+  }
 }
