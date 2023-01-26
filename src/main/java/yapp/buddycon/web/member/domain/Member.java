c8ebb38908f185bd2082d9263aec557e3b22ce81
yubin <dbqls2821@gmail.com>
2023-01-26 23:45:03 +0900

    feat: Member entity에 builder 추가

--- src/main/java/yapp/buddycon/web/member/domain/Member.java
+++ src/main/java/yapp/buddycon/web/member/domain/Member.java
@@ -1,6 +1,7 @@
 package yapp.buddycon.web.member.domain;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.NoArgsConstructor;
 import yapp.buddycon.common.domain.BaseEntity;
 
@@ -9,12 +10,32 @@ import javax.persistence.*;
 @Entity
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class Member extends BaseEntity {
 
   @Column(name = "client_id", unique = true)
   private Long clientId;
 
+  @Column(name = "email", nullable = false)
+  private String email;
+
   @Column(name = "name")
   private String name;
+
+  @Column(name = "gender")
+  private String gender;
+
+  @Column(name = "age_range")
+  private String ageRange;
+
+  public static Member create(Long clientId, String email, String name, String gender, String ageRange) {
+    return Member.builder()
+      .clientId(clientId)
+      .email(email)
+      .name(name)
+      .gender(gender)
+      .ageRange(ageRange)
+      .build();
+  }
 
 }
