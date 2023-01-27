89c583045dd4b5d4f4fd5836f4f46b679c284837
yubin <dbqls2821@gmail.com>
2023-01-27 19:02:15 +0900

    feat: CouponUseCase, CouponService 내부 구현

--- src/main/java/yapp/buddycon/web/coupon/application/port/in/CouponUseCase.java
+++ src/main/java/yapp/buddycon/web/coupon/application/port/in/CouponUseCase.java
@@ -1,4 +1,15 @@
 package yapp.buddycon.web.coupon.application.port.in;
 
+import org.springframework.data.domain.Pageable;
+import yapp.buddycon.web.auth.adapter.out.AuthMember;
+import yapp.buddycon.web.coupon.adapter.in.response.CouponsResponseDto;
+
+import java.util.List;
+
 public interface CouponUseCase {
+
+  List<CouponsResponseDto> getSortedGifticons(boolean usable, Pageable pageable, AuthMember authMember);
+
+  List<CouponsResponseDto> getSortedCustomCoupons(boolean usable, Pageable pageable, AuthMember authMember);
+
 }
--- src/main/java/yapp/buddycon/web/coupon/application/service/CouponService.java
+++ src/main/java/yapp/buddycon/web/coupon/application/service/CouponService.java
@@ -1,10 +1,15 @@
 package yapp.buddycon.web.coupon.application.service;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import yapp.buddycon.web.auth.adapter.out.AuthMember;
+import yapp.buddycon.web.coupon.adapter.in.response.CouponsResponseDto;
 import yapp.buddycon.web.coupon.application.port.in.CouponUseCase;
 import yapp.buddycon.web.coupon.application.port.out.CouponCommandPort;
 import yapp.buddycon.web.coupon.application.port.out.CouponQueryPort;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -13,5 +18,23 @@ public class CouponService implements CouponUseCase {
   private final CouponCommandPort couponCommandPort;
   private final CouponQueryPort couponQueryPort;
 
+  @Override
+  public List<CouponsResponseDto> getSortedGifticons(boolean usable, Pageable pageable, AuthMember authMember) {
+    if(usable) return couponQueryPort.findUsableGifticonsSortedBy(pageable, authMember.id());
+    return couponQueryPort.findUsedGifticonsSortedBy(pageable, authMember.id());
+  }
+
+  @Override
+  public List<CouponsResponseDto> getSortedCustomCoupons(boolean usable, Pageable pageable, AuthMember authMember) {
+    if(usable) return couponQueryPort.findUsableCustomCouponsSortedBy(pageable, authMember.id());
+    return couponQueryPort.findUsedCustomCouponsSortedBy(pageable, authMember.id());
+  }
+
+  /*
+  유효기간순 -> expireDate ASC
+  등록순 -> createdAt ASC
+  이름순 -> name ASC
+  page=3&size=10&sort=couponInfo.expireDate,DESC
+  */
 
 }
