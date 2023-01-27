86764aca4ee0e7bbef625499518fe6b6e9205faa
yubin <dbqls2821@gmail.com>
2023-01-27 19:03:13 +0900

    feat: CouponQueryPort, CouponQueryRepository 내부 구현

--- src/main/java/yapp/buddycon/web/coupon/adapter/out/CouponQueryRepository.java
+++ src/main/java/yapp/buddycon/web/coupon/adapter/out/CouponQueryRepository.java
@@ -1,14 +1,39 @@
 package yapp.buddycon.web.coupon.adapter.out;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Repository;
+import yapp.buddycon.web.coupon.adapter.in.response.CouponsResponseDto;
 import yapp.buddycon.web.coupon.application.port.out.CouponQueryPort;
 import yapp.buddycon.web.coupon.domain.CouponState;
+
+import java.util.List;
 
 @Repository
 @RequiredArgsConstructor
 public class CouponQueryRepository implements CouponQueryPort {
 
   private final CouponJpaRepository couponJpaRepository;
+
+  @Override
+  public List<CouponsResponseDto> findUsableGifticonsSortedBy(Pageable pageable, Long memberId) {
+    return couponJpaRepository.findSortedGifticonsAccordingToState(CouponState.USABLE, memberId, pageable);
+  }
+
+  @Override
+  public List<CouponsResponseDto> findUsedGifticonsSortedBy(Pageable pageable, Long memberId) {
+    return couponJpaRepository.findSortedGifticonsAccordingToState(CouponState.USED, memberId, pageable);
+  }
+
+  @Override
+  public List<CouponsResponseDto> findUsableCustomCouponsSortedBy(Pageable pageable, Long memberId) {
+    return couponJpaRepository.findSortedCustomCouponsAccordingToState(CouponState.USABLE, memberId, pageable);
+  }
+
+  @Override
+  public List<CouponsResponseDto> findUsedCustomCouponsSortedBy(Pageable pageable, Long memberId) {
+    return couponJpaRepository.findSortedCustomCouponsAccordingToState(CouponState.USED, memberId, pageable);
+  }
+
 
 }
--- src/main/java/yapp/buddycon/web/coupon/application/port/out/CouponQueryPort.java
+++ src/main/java/yapp/buddycon/web/coupon/application/port/out/CouponQueryPort.java
@@ -1,4 +1,15 @@
 package yapp.buddycon.web.coupon.application.port.out;
 
+import org.springframework.data.domain.Pageable;
+import yapp.buddycon.web.coupon.adapter.in.response.CouponsResponseDto;
+
+import java.util.List;
+
 public interface CouponQueryPort {
+
+  List<CouponsResponseDto> findUsableGifticonsSortedBy(Pageable pageable, Long memberId);
+  List<CouponsResponseDto> findUsedGifticonsSortedBy(Pageable pageable, Long memberId);
+
+  List<CouponsResponseDto> findUsableCustomCouponsSortedBy(Pageable pageable, Long memberId);
+  List<CouponsResponseDto> findUsedCustomCouponsSortedBy(Pageable pageable, Long memberId);
 }
