774e5dfe59fa5f333427a832db61a532c59f41fc
yubin <dbqls2821@gmail.com>
2023-01-30 17:32:22 +0900

    fix: response에 coupon id 추가

--- src/main/java/yapp/buddycon/web/coupon/adapter/out/CouponJpaRepository.java
+++ src/main/java/yapp/buddycon/web/coupon/adapter/out/CouponJpaRepository.java
@@ -9,7 +9,7 @@ import yapp.buddycon.web.coupon.domain.Coupon;
 public interface CouponJpaRepository extends JpaRepository<Coupon, Long> {
 
   @Query(value = """
-    select new yapp.buddycon.web.coupon.adapter.in.response.GifticonInfoResponseDto(c.couponInfo.imageUrl, c.couponInfo.barcode, c.couponInfo.name, c.couponInfo.expireDate, c.couponInfo.storeName, c.couponInfo.memo, c.couponInfo.isMoneyCoupon, c.couponInfo.leftMoney)
+    select new yapp.buddycon.web.coupon.adapter.in.response.GifticonInfoResponseDto(c.id, c.couponInfo.imageUrl, c.couponInfo.barcode, c.couponInfo.name, c.couponInfo.expireDate, c.couponInfo.storeName, c.couponInfo.memo, c.couponInfo.isMoneyCoupon, c.couponInfo.leftMoney)
     from Coupon c
     where c.member.id = :memberId
     and c.id = :id
@@ -18,7 +18,7 @@ public interface CouponJpaRepository extends JpaRepository<Coupon, Long> {
   GifticonInfoResponseDto findGifticonByMemberIdAndIdAndCouponType(Long memberId, Long id);
 
   @Query(value = """
-    select new yapp.buddycon.web.coupon.adapter.in.response.CustomCouponInfoResponseDto(c.couponInfo.imageUrl, c.couponInfo.barcode, c.couponInfo.name, c.couponInfo.expireDate, c.couponInfo.storeName, c.couponInfo.sentMemberName, c.couponInfo.memo)
+    select new yapp.buddycon.web.coupon.adapter.in.response.CustomCouponInfoResponseDto(c.id, c.couponInfo.imageUrl, c.couponInfo.barcode, c.couponInfo.name, c.couponInfo.expireDate, c.couponInfo.storeName, c.couponInfo.sentMemberName, c.couponInfo.memo)
     from Coupon c
     where c.member.id = :memberId
     and c.id = :id
