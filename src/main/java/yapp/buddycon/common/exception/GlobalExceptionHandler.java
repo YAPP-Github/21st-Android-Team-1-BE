131d3b64cad3921185ac88b64f455e7bde54a225
yubin <dbqls2821@gmail.com>
2023-02-01 22:20:10 +0900

    feat: 영속성 exception global handler 메소드 추가

--- src/main/java/yapp/buddycon/common/exception/GlobalExceptionHandler.java
+++ src/main/java/yapp/buddycon/common/exception/GlobalExceptionHandler.java
@@ -1,10 +1,13 @@
 package yapp.buddycon.common.exception;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import javax.persistence.PersistenceException;
 
 @Slf4j
 @RestControllerAdvice
@@ -15,4 +18,14 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
     log.error("handleCustomException throw CustomException : {}", e.getErrorCode());
     return ErrorResponse.toResponseEntity(e.getErrorCode());
   }
+
+  @ExceptionHandler(value = { PersistenceException.class })
+  protected ResponseEntity<ErrorResponse> handlePersistenceException(PersistenceException e) {
+    log.error("handlePersistenceException throw PersistenceException : {}", e.getMessage());
+    final ErrorResponse errorResponse = ErrorResponse.builder()
+      .code("DatabaseException")
+      .message("database error").build();
+    return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorResponse);
+  }
 }
+
