cfc550bb935bae2f10f38203a7abc5dc7c8464b2
Huey-J <wkdgus7113@gmail.com>
2023-02-02 22:11:03 +0900

    feat: AWS S3 파일 업로드 메소드 추가

---
+++ src/main/java/yapp/buddycon/common/util/AwsS3FileProvider.java
@@ -0,0 +1,34 @@
+package yapp.buddycon.common.util;
+
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.amazonaws.services.s3.model.PutObjectResult;
+import java.io.IOException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
+
+@RequiredArgsConstructor
+@Component
+public class AwsS3FileProvider {
+
+  private final AmazonS3 s3Client;
+
+  @Value("${cloud.s3.bucket}")
+  private String BUCKET_NAME;
+  @Value("${cloud.s3.path.root}")
+  private String PATH_ROOT;
+
+  public String upload(MultipartFile file) throws IOException {
+    String s3PathKey = PATH_ROOT + "/" + UuidProvider.generateUuid();
+    ObjectMetadata metaData = new ObjectMetadata();
+    metaData.setContentLength(file.getSize());
+    metaData.setContentType(file.getContentType());
+    PutObjectResult result = s3Client
+        .putObject(new PutObjectRequest(BUCKET_NAME, s3PathKey, file.getInputStream(), metaData));
+    return s3PathKey;
+  }
+
+}
