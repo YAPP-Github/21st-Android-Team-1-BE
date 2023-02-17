7c23705f1c92aea2ea5e3053aafb981b3eca4035
huey-J <wkdgus7113@gmail.com>
2023-02-17 15:56:54 +0900

    feat: 바코드 생성 클래스 생성

---
+++ src/main/java/yapp/buddycon/common/util/BarcodeNumberProvider.java
@@ -0,0 +1,25 @@
+package yapp.buddycon.common.util;
+
+import java.util.Random;
+
+public class BarcodeNumberProvider {
+
+  public String createRandomBarcode() {
+    int leftLimit = 48; // numeral '0'
+    int rightLimit = 57; // letter '9'
+    int[] lengthList = {4, 4, 7};
+
+    Random random = new Random();
+    String generatedString = "";
+
+    for (int length : lengthList) {
+      generatedString += random.ints(leftLimit, rightLimit + 1)
+          .limit(length)
+          .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
+          .toString();
+      generatedString += "-";
+    }
+    return generatedString.substring(0, generatedString.length() - 1);
+  }
+
+}
